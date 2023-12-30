9708e9162c30f14ab14bdaac54d6634035c1e1e9
subhadeephazra <subhadeephazra68@gmail.com>
2023-12-30 23:14:46 +0530

    watchlist update

--- src/pages/watchlist.js
+++ src/pages/watchlist.js
@@ -7,53 +7,54 @@ import TabsComponent from "../components/Dashboard/Tabs/tabs";
 import { get100Coins } from "../functions/get100Coins";
 
 function WatchlistPage() {
-  // const coins = JSON.parse(localStorage.getItem("watchlist"));
-  // const [myWatchlist, setMyWatchlist] = useState([]);
-  // const [loading, setLoading] = useState(false);
+  const coins = JSON.parse(localStorage.getItem("watchlist"));
+  const [myWatchlist, setMyWatchlist] = useState([]);
+  const [loading, setLoading] = useState(false);
 
-  // useEffect(() => {
-  //   getData();
-  // }, []);
+  useEffect(() => {
+    getData();
+  }, []);
 
-  // const getData = async () => {
-  //   setLoading(true);
-  //   const allCoins = await get100Coins();
-  //   if (coins) {
-  //     setMyWatchlist(allCoins.filter((item) => coins.includes(item.id)));
-  //   }
-  //   setLoading(false);
-  // };
+  const getData = async () => {
+    setLoading(true);
+    const allCoins = await get100Coins();
+    if (coins) {
+      setMyWatchlist(allCoins.filter((item) => coins.includes(item.id)));
+    }
+    setLoading(false);
+  };
 
   return (
-    // <div>
-    //   {loading || !coins ? (
-    //     <Loader />
-    //   ) : (
-    //     <div style={{ minHeight: "90vh" }}>
-    //       {myWatchlist?.length == 0 || !coins ? (
-    //         <div>
-    //           <Header />
-    //           <h1 style={{ textAlign: "center", marginBottom: "2rem" }}>
-    //             No Items in the Watchlist
-    //           </h1>
-    //           <div style={{ display: "flex", justifyContent: "center" }}>
-    //             <a href="/dashboard">
-    //               <Button text={"Dashboard"} />
-    //             </a>
-    //           </div>
-    //         </div>
-    //       ) : (
-    //         <div style={{ height: "95vh" }}>
-    //           <Header />
-    //           <TabsComponent coins={myWatchlist} isWatchlistPage={true} />
-    //         </div>
-    //       )}
-    //     </div>
-    //   )}
-    // </div>
+  <div>
     <div>
-      <Header/>
-      <h1 style={{ display: 'flex', justifyContent: 'center' }}>Page Under Construction !</h1>
+      {loading || !coins ? (
+        <Loader />
+      ) : (
+        <div style={{ minHeight: "90vh" }}>
+          {myWatchlist?.length == 0 || !coins ? (
+            <div>
+              <Header />
+              <h1 style={{ textAlign: "center", marginBottom: "2rem" }}>
+                No Items in the Watchlist
+              </h1>
+              <div style={{ display: "flex", justifyContent: "center" }}>
+                <a href="/dashboard">
+                  <Button text={"Dashboard"} />
+                </a>
+              </div>
+            </div>
+          ) : (
+            <div style={{ height: "95vh" }}>
+              <Header />
+              <TabsComponent coins={myWatchlist} isWatchlistPage={true} />
+            </div>
+          )}
+        </div>
+      )}
+    </div>
+
+      {/* <Header/> */}
+      {/* <h1 style={{ display: 'flex', justifyContent: 'center' }}>Page Under Construction !</h1>
 
        <video 
          src="https://cdnl.iconscout.com/lottie/premium/preview-watermark/website-under-construction-7362406-6031671.mp4" 
@@ -63,7 +64,7 @@ function WatchlistPage() {
          playsinline="" 
          type="video/mp4" 
          style={{ width: '100vw', height: '68vh' }}
-       ></video>
+       ></video> */}
     </div>
   );
 }
